94e0c730133a43c7ba073e06c18726d287a6cb4e
TheKiller65YT <jackgonser@gmail.con>
2018-11-24 19:35:08 -1000

    Remeber to save files...

--- src/main/java/us/physicsarebad/mafiaGame/mafiaGame.java
+++ src/main/java/us/physicsarebad/mafiaGame/mafiaGame.java
@@ -1,5 +1,7 @@
 package us.physicsarebad.mafiaGame;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class mafiaGame extends JavaPlugin{
@@ -13,6 +15,16 @@ public class mafiaGame extends JavaPlugin{
     @Override
     public void onDisable() {
         getLogger().info("Thank you for using the Mafia Game developed by PhysicsareBad.\nVist us at anytime at physicsarebad.us!");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following, note: If you only registered this executor for one command, you don't need this
+    		// doSomething
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the value of false will be returned.
+    	return false; 
     }
     
     //TODO Define all roles
