118238ea114d2b6605109e427cae5c0cd2d5e699
Jack Gonser <jackgonser@JTs-Macbook.home>
2018-11-24 19:15:23 -1000

    Define TODOs and basic logging

--- src/main/java/us/physicsarebad/mafiaGame/mafiaGame.java
+++ src/main/java/us/physicsarebad/mafiaGame/mafiaGame.java
@@ -5,13 +5,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class mafiaGame extends JavaPlugin{
 	@Override
     public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
-		getLogger().info("Mafia Plugin Developed by PhysicsAreBad.");
-		getLogger().info("Source Code avaliable ");
+		//TODO Add Listeners for player numbers so can gauge when to game init
+		getLogger().info(" Mafia Plugin Developed by PhysicsAreBad.\nSource Code avaliable at https://github.com/TheKiller65YT/mafiaPlugin/\nYou can get support by emailing pluginsupport@physicsarebad.us");
+		getLogger().info("This plugin is not reload friendly currently, so please restart the server if you changed anything.");
     }
     
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+        getLogger().info("Thank you for using the Mafia Game developed by PhysicsareBad.\nVist us at anytime at physicsarebad.us!");
     }
+    
+    //TODO Define all roles
 }
